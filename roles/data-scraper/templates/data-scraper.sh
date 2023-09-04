0932bac6b24a624a4cd6859522c14d92109546b9
slezarikm <michal@slezarik.sk>
2023-09-04 13:15:43 +0200

    data scraper avg load added

--- roles/data-scraper/templates/data-scraper.sh
+++ roles/data-scraper/templates/data-scraper.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 curl -XGET localhost:8880/nginx_status >> /tmp/nginx-status.log
+cat /proc/loadavg | awk '{ print "{ \"load1\":"$1,",\n  \"load5\":" $2,",\n  \"load15:\":" $3 "\n}" }' >> /tmp/system-load.log
 sleep 30
