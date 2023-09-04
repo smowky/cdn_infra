68cc60be13247361d71afc3841582e4886aa5aea
slezarikm <michal@slezarik.sk>
2023-09-04 16:34:32 +0200

    data scraper update

--- roles/data-scraper/files/data-scraper.sh
+++ roles/data-scraper/files/data-scraper.sh
@@ -5,5 +5,5 @@ system_load_log='/tmp/system-load.log'
 
 
 curl -XGET localhost:8880/nginx_status >> ${nginx_status_log}
-cat /proc/loadavg | awk -v date="$(date +%F_%T )" '{ print "{ \"load1\":"$1,",\n  \"load5\":" $2,",\n  \"load15:\":" $3,",\n  \"date\": \"" date"\"  }"  }' >> ${system_load_log}
+cat /proc/loadavg | awk -v date="$(date +%F_%T )" '{ print "{ \"load1\":"$1,",\n  \"load5\":" $2,",\n  \"load15:\":" $3,",\n  \"date\": \"" date"\"\n}"  }' >> ${system_load_log}
 sleep 30
